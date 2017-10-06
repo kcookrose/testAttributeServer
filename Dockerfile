5952fbb88f184912ef0ddc22445c06a5fead03e6
Jonas Schumacher <jonas.merlin.schumacher@mailbox.org>
2017-10-06 10:02:50 +0200

    some tests gone wrong

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,7 @@
-FROM tensorflow/tensorflow
-
-RUN apt-get update && apt-get install -y --no-install-recommends \
-	curl \
-        libfreetype6-dev \
-        libpng12-dev \
-        libzmq3-dev \
-        pkg-config \
-        python-numpy \
-        python-pip \
-        python-scipy \
-        git \
-        libhdf5-dev \
-        graphviz \
-        && \
-    	apt-get clean && \
-	rm -rf /var/lib/apt/lists/*
-
-RUN pip install keras
-
-RUN apt-get install python3 
+FROM floydhub/dl-docker:cpu
 
 COPY keras.json /root/.keras/keras.json
 
 ADD . /app
 
 WORKDIR /app
-
-EXPOSE 5000
-
-CMD ["python", "./classifiers/test.py"]
-
-
-
--- classifiers/test.py
+++ classifiers/test.py
@@ -3,7 +3,7 @@ import argparse
 import numpy as np
 from PIL import Image
 from io import BytesIO
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 from keras.preprocessing import image
 from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
@@ -20,20 +20,20 @@ def predict(model, img, target_size, top_n=3):
     preds = model.predict(x)
     return decode_predictions(preds, top=top_n)[0]
 
-def plot_preds(image, preds):
-    plt.imshow(image)
-    plt.axis('off')
-
-    plt.figure()
-    order = list(reversed(range(len(preds))))
-    bar_preds = [pr[2] for pr in preds]
-    labels = (pr[1] for pr in preds)
-    plt.barh(order, bar_preds, alpha=0.5)
-    plt.yticks(order, labels)
-    plt.xlabel('Probability')
-    plt.xlim(0, 1.01)
-    plt.tight_layout()
-    plt.show()
+# def plot_preds(image, preds):
+#     plt.imshow(image)
+#     plt.axis('off')
+#
+#     plt.figure()
+#     order = list(reversed(range(len(preds))))
+#     bar_preds = [pr[2] for pr in preds]
+#     labels = (pr[1] for pr in preds)
+#     plt.barh(order, bar_preds, alpha=0.5)
+#     plt.yticks(order, labels)
+#     plt.xlabel('Probability')
+#     plt.xlim(0, 1.01)
+#     plt.tight_layout()
+#     plt.show()
 
 if __name__=="__main__":
   a = argparse.ArgumentParser()
@@ -43,9 +43,7 @@ if __name__=="__main__":
   if args.image is not None:
     img = Image.open(args.image)
     preds = predict(model, img, target_size)
-    plot_preds(img, preds)
+    print(preds)
   else:
       a.print_help()
       sys.exit(1)
-
-plot_preds(img, preds)
