e78a745216abfd5c12536a54d9d19bf4d61514ac
Jonas Schumacher <jonasmerlin@ThinPad.home>
2017-10-28 21:22:00 +0200

    cli now runs gender claissifer, can  potentially run arbitrary number of classifiers

--- code/app/main.py
+++ code/app/main.py
@@ -2,9 +2,15 @@ from arguments import set_up_arguments
 import os
 import subprocess
 
+import classifiers
+
 args = set_up_arguments()
 
-with open(args.portrait) as portrait:
-    for root, dirs, files in os.walk("./classifiers/"):
-        for file in files:
-            subprocess.check_output("python {0} --image {1}".format(os.path.abspath(os.path.join(root, file)), os.path.abspath("./{0}".format(args.portrait))))
+print(classifiers.modules)
+#with open(args.portrait) as portrait:
+for module in classifiers.modules:
+	print(dir(module))
+	module.classify(os.path.abspath(args.portrait))
+#    for root, dirs, files in os.walk("./classifiers/"):
+#        for file in files:
+#            subprocess.check_output("python {0} --image {1}".format(os.path.abspath(os.path.join(root, file)), os.path.abspath("./{0}".format(args.portrait))))
